f3c4a907640faea0af63a97fb3180c479c07670d
munah <munah1917@outlook.com>
2022-09-15 16:24:23 +0100

    Added Styles + Dice Component

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react"
+import Dice from "./components/Dice"
 
-export default function App () {
+export default function App() {
   return (
-    <main>
-        Something
-    </main>
+      <main >
+          <div className="dice-container">
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+              <Dice value={Math.floor(Math.random()*10)}/>
+          </div>
+      </main>
   )
 }
