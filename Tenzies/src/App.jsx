b2ffad69eb97c17d0c4d8a3d055f06a7646e5a09
munah <munah1917@outlook.com>
2022-09-15 16:50:39 +0100

    Adding states for random numbers

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -2,19 +2,30 @@ import React from "react"
 import Dice from "./components/Dice"
 
 export default function App() {
+
+  function allNewDice() {
+    const randomTen = []
+    
+    for(let i = 0; i < 10; i++){
+        // Math.ceil will start from 1 instead of 0 like Math.floor - since dice dont have 0
+        randomTen.push(Math.ceil(Math.random()*6));
+    }
+
+    return randomTen;
+  }
+
+  const [diceNumbers, setDiceNumbers] = React.useState(allNewDice);
+    
+    const diceNumberElements = diceNumbers.map((num) => {
+        return(
+            <Dice value={num}/>
+        )
+    })
+
   return (
       <main >
           <div className="dice-container">
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
-              <Dice value={Math.floor(Math.random()*6)}/>
+              {diceNumberElements}
           </div>
       </main>
   )
