1ba110ef8ea2975d21b91d63acb82ebce27eee6e
munah <munah1917@outlook.com>
2022-09-16 17:56:17 +0100

    Added Functionality: Dice colors change when clicked

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -4,40 +4,57 @@ import {nanoid} from "nanoid"
 
 export default function App() {
 
-  function allNewDice() {
-    const randomTen = []
-    
-    for(let i = 0; i < 10; i++){
-        // Math.ceil will start from 1 instead of 0 like Math.floor - since dice dont have 0
-        randomTen.push({
-          value: Math.ceil(Math.random()*6),
-          isHeld: false,
-          id: nanoid()
-        });
+    const [dice, setDice] = React.useState(allNewDice);
+
+    function allNewDice() {
+        const randomTen = []
+        
+        for(let i = 0; i < 10; i++){
+            // Math.ceil will start from 1 instead of 0 like Math.floor - since dice dont have 0
+            randomTen.push({
+              value: Math.ceil(Math.random()*6),
+              isHeld: false,
+              id: nanoid()
+            });
+        }
+
+        return randomTen;
     }
-
-    return randomTen;
-  }
-
-  const [diceNumbers, setDiceNumbers] = React.useState(allNewDice);
-    
-    const diceNumberElements = diceNumbers.map((num) => {
-        return(
-            <Dice key={num.id} value={num.value}/>
-        )
-    })
 
     function rollDice() {
-      setDiceNumbers(allNewDice());
+        setDice(allNewDice());
     }
 
-  return (
-      <main >
-          <div className="dice-container">
-              {diceNumberElements}
-          </div>
+    function hold(id) {
+        
+        const updatedDice = dice.map(dice => {
+            return {
+              ...dice,
+              isHeld: (id === dice.id) ? !dice.isHeld : dice.isHeld,
+            }
+        })
 
-          <button className="button-roll" onClick={rollDice}> Roll </button>
-      </main>
-  )
+        setDice(updatedDice)
+      
+    }
+
+    
+      
+      const diceElements = dice.map((dice) => {
+          return(
+              <Dice key={dice.id} value={dice.value} isHeld={dice.isHeld} clickHandler={()=>{hold(dice.id)}}/>
+          )
+      })
+
+      
+
+    return (
+        <main >
+            <div className="dice-container">
+                {diceElements}
+            </div>
+
+            <button className="button-roll" onClick={rollDice}> Roll </button>
+        </main>
+    )
 }
--- Tenzies/src/components/Dice.jsx
+++ Tenzies/src/components/Dice.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 
 export default function Dice(props) {
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
     return(
-        <div className="dice-boxes">
+        <div className="dice-boxes" style={styles} onClick={props.clickHandler}>
             <h1 className="dice-num">{props.value}</h1>
         </div>
     )
